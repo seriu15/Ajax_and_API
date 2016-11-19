551d80f4639db377b565056d12782dd2c20e091c
Sebastian Rojek <seriu54@gmail.com>
2016-11-19 19:30:52 +0100

    Change to exercise 11.5

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
+$(function() {
+	var tweetLink = "https://twitter.com/intent/tweet?text=";
+	var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
-var url = 'http://api.icndb.com/jokes/random';
+	function getQuote() {
+		$.getJSON(quoteUrl, createTweet);
+	}
 
-var button = document.getElementById('get-joke');
-button.onload =  getJoke(); 
-button.addEventListener('click', function(){
-	getJoke();
-});
-var paragraph = document.getElementById('joke');
+	function createTweet(input) {
+		if (!input.quoteAuthor.length) {
+			input.quoteAuthor = "Unknown author";
+		}
+		var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+		if(tweetText.length > 140){
+			getQuote();
+		} else {
+			var tweet = tweetLink + encodeURIComponent(tweetText);
+			$('.quote').text(input.quoteText);
+			$('.author').text("Author: " + input.quoteAuthor);
+			$('.tweet').attr('href', tweet);
+		}
+		$('.tweet').attr('href', tweet);
+	}
 
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
 	});
-	xhr.send();
-}
+});
