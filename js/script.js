da2b6e520543c0c63d9d876d3339a5788fee51a4
Sebastian Rojek <seriu54@gmail.com>
2016-11-19 18:27:13 +0100

    Add exercise 11.4

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById('get-joke');
+button.onload =  getJoke(); 
+button.addEventListener('click', function(){
+	getJoke();
+});
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function(){
+		var response = JSON.parse(xhr.response);
+		paragraph.innerText = response.value.joke;
+	});
+	xhr.send();
+}
